a2c61534b7b546caf2143036140162b25ee492fc
Matt Hilton <matt.hilton@mykolab.com>
2025-10-23 09:52:41 +0200

    Version bump

--- astLib/__init__.py
+++ astLib/__init__.py
@@ -3,7 +3,7 @@ astLib - python astronomy modules
 
 (c) 2007-2012 Matt Hilton
 
-(c) 2013-2024 Matt Hilton & Steven Boada
+(c) 2013-2025 Matt Hilton & Steven Boada
 
 U{https://astlib.readthedocs.io}
 
@@ -13,4 +13,4 @@ See the LICENSE file for information on distribution.
 """
 
 __all__=['astCalc', 'astCoords', 'astImages', 'astPlots', 'astStats', 'astWCS', 'astSED']
-__version__ = '0.12.0'
+__version__ = '0.13.0'
